0dbdf9fc1da18f7cbf0f0e8bfe6b00ee53c1d565
Jay Wilder <jayswilder@yahoo.com>
2020-12-13 19:08:33 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import EventCalendar from './EventCalendar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <EventCalendar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/EventCalendar.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list'
+import './App.css'
+import { Button, TextField } from '@material-ui/core'
+
+
+export default class EventCalendar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            eventsArr: [],
+            eventTitle: '',
+            startTime: '',
+            endTime: ''
+        }
+    }
+
+    newEventTitle = event => {
+        const value = event.target.value
+        this.setState({
+            eventTitle: value
+        })
+    }
+
+    newStartTime = event => {
+        const value = event.target.value
+        this.setState({
+            startTime: value
+        })
+    }
+
+    newEndTime = event => {
+        const value = event.target.value
+        this.setState({
+            endTime: value
+        })
+    }
+
+    addEventButton = () => {
+        this.state.eventsArr.push({ title: this.state.eventTitle, start: this.state.startTime, end: this.state.endTime })
+        console.log("events: ", this.state.eventsArr)
+    }
+
+    componentDidUpdate(pP, pS, sS) {
+        if (pS.Calendar.events.length < this.state.eventsArr.length) {
+            this.setState({
+                eventsArr: this.state.eventsArr
+            })
+        }
+    }
+
+
+    render() {
+        return (
+            <div id="calendar-container">
+                <div id='calendar'>
+                    <FullCalendar
+                        editable={true}
+                        plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+                        initialView="dayGridMonth"
+                        events={this.state.eventsArr}
+                        weekends={false}
+                        height="600px"
+                        headerToolbar={{
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'dayGridMonth,dayGridWeek,dayGridDay,listWeek'
+                        }}
+                    />
+                </div>
+                <div>
+                    <form className="form" noValidate autoComplete="off">
+                        <TextField id="standard-basic" label="Description" onChange={this.newEventTitle} /><br></br><br></br>
+                        <TextField
+                            id="start-time"
+                            label="Start Date"
+                            type="date"
+                            InputLabelProps={{
+                                shrink: true,
+                            }} onChange={this.newStartTime}
+                        /><br></br><br></br>
+                        <TextField
+                            id="end-time"
+                            label="End Date"
+                            type="date"
+                            InputLabelProps={{
+                                shrink: true,
+                            }} onChange={this.newEndTime}
+                        /><br></br><br></br>
+                        <Button variant="contained" color="primary" onClick={this.addEventButton}>
+                            Add Event
+                    </Button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
